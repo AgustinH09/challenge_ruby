56ac90248e98f82cbcb346468e02495760133199
AgustinH09 <agus.hernandez9@gmail.com>
2025-09-10 18:40:05 -0300

    Add method to calculate sales taxes and total price of receipt items & specs for this

--- lib/receipt.rb
+++ lib/receipt.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'tax_calculator'
+
 # Receipt class that holds a list of items
 class Receipt
   attr_reader :items
@@ -10,5 +12,16 @@ class Receipt
 
   def add_item(item)
     items << item
+  end
+
+  def total_sales_tax
+    @items.sum do |item|
+      TaxCalculator.calculate(item) * item.quantity
+    end.round(2)
+  end
+
+  def total_price
+    total = @items.sum { |item| item.price * item.quantity }
+    total + total_sales_tax
   end
 end
--- spec/receipt_spec.rb
+++ spec/receipt_spec.rb
@@ -2,6 +2,7 @@
 
 require 'receipt'
 require 'item'
+require 'tax_calculator'
 
 RSpec.describe Receipt do
   describe '#initialize' do
@@ -19,6 +20,24 @@ RSpec.describe Receipt do
       receipt.add_item(item)
 
       expect(receipt.items).to include(item)
+    end
+  end
+
+  describe 'total calculations' do
+    let(:receipt) { Receipt.new }
+
+    before do
+      receipt.add_item(Item.new(quantity: 2, name: 'book', price: 12.49))
+      receipt.add_item(Item.new(quantity: 1, name: 'music CD', price: 14.99))
+      receipt.add_item(Item.new(quantity: 1, name: 'chocolate bar', price: 0.85))
+    end
+
+    it 'calculates the correct total sales tax' do
+      expect(receipt.total_sales_tax).to eq(1.50)
+    end
+
+    it 'calculates the correct grand total' do
+      expect(receipt.total_price).to eq(42.32)
     end
   end
 end
